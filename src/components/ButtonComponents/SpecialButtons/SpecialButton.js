09e6270b05ba70fda54d64a27b82017c13852e00
aaamg <andrewmanuelgarcia@gmail.com>
2019-09-10 17:07:54 -0400

    added props

--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -4,7 +4,7 @@ const SpecialButton = (props) => {
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-      {props.specialBut}
+      <button>{props.specialBut}</button>
     </>
   );
 };
